cdf7bec194dea2ad31492d1c8f07120d0e24ec75
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2020-03-13 09:19:19 -0300

    Criando pessoa model vazio

---
+++ TesteDePerformance03/Models/PessoaModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteDePerformance03.Models
+{
+    public class PessoaModel
+    {
+    }
+}
